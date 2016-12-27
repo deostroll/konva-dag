1f42bf9ade7b5b14459bf72abc6a8072413d3f84
arun_jayapal <arun_jayapal@infosys.com>
2016-12-27 10:56:54 +0530

    dag positioning

--- app/graph.js
+++ app/graph.js
@@ -1,4 +1,13 @@
 (function(Konva) {
+  Konva.Util.$extend = function(target, source) {
+    for (var key in source) {
+      if (source.hasOwnProperty(key)) {
+        target[key] = source[key]
+      }
+    }
+    return target;
+  };
+
   Konva.Dag = function(dag, options) {
     Konva.Group.call(this, options);
     this._dag = dag;
@@ -7,8 +16,24 @@
 
   Konva.Dag.prototype = {
     $init: function(config) {
+      var self = this;
       var nodes = this._dag.nodes;
+      // debugger;
+      var circles = nodes.map((n, i) => {
 
+        var opts = Konva.Util.$extend({
+          x: i * 35,
+          y: 0
+        }, config.c);
+
+        console.log(opts);
+        var c = new Konva.Circle(opts);
+        return c;
+      });
+
+      this.add.apply(this, circles);
     }
-  }
+
+  };
+  Konva.Util.extend(Konva.Dag, Konva.Group);
 })(Konva)
--- app/main.js
+++ app/main.js
@@ -27,5 +27,22 @@ window.addEventListener('load', function() {
   edges.forEach(edg => dag.connect(edg[0], edg[1]));
   console.log('Nodes:', dag.toString());
   console.log('Edges:', dag.getEdges().toString());
+  var dag = new Konva.Dag(dag, {
+    c: {
+      fill: 'lightgreen',
+      radius: 15
+    }
+  });
+  var dagRect = dag.getClientRect();
+  console.log('Dag Rect:', dagRect);
+  dag.offset({
+    x: dagRect.width/2,
+    y: dagRect.height/2
+  });
+  dag.position({
+    x: 200, y: 150
+  });
+  
+  layer.add(dag);
   stage.add(layer);
 }, false);
